b3b7b57eddd8c6d4cfc5328a744d38ad1918bca6
Akshat Sharma <akshatcoder@outlook.com>
2022-01-21 23:55:08 +0530

    Completed the code in Factorial method

--- Functions/Factorial.java
+++ Functions/Factorial.java
@@ -1,1 +1,28 @@
+package com.akshat;
 
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main (String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println ("Enter a number");
+        int n = in.nextInt ();
+        if (n == 0)
+        {
+            System.out.println (n + "! = 0");
+        }
+        else
+            System.out.println (Factorial (n));
+
+    }
+    static int Factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i < n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+
+}
